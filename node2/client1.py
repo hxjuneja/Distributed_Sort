cfa152da3819aab3d337b23aed61860e1a1cb395
hardikj <hardikjuneja.hj@gmail.com>
2014-05-05 04:06:09 +0530

    commting code from node2

--- node2/client1.py
+++ node2/client1.py
@@ -1,28 +1,46 @@
 import zmq
 import sys
 
-context = zmq.Context()
 
-# connect to server
-sock = context.socket(zmq.REQ)
-sock.connect("tcp://172.16.86.84:5555")
+class ClientCode():
+    
+    def __init__(self):
+        context = zmq.Context()
 
-# Open file
-fo = open("../data/data1.txt", "r+")
-fo2 = open("dataB.txt", "w+")
+        # connect to server
+        self.sock = context.socket(zmq.REQ)
+        self.sock.connect("tcp://172.16.86.84:5555")
 
-data = fo.read().split("\n")
+        # Open file
+        self.fo = open("../data/data1.txt", "r+")
+        sefl.fo2 = open("dataB.txt", "a+")
 
-for i in data:
-    field = i.split(" ")
-    if len(field) > 1:
-        if str(field[1]) == 'B':
-             wf = " ".join(field)
-             wf = wf + "\n"
-             fo2.write(wf)
-        else:
-             wf = " ".join(field)
-             wf = wf + "\n"
-             sock.send("Sending")
-             print("sent to client one")
-             print sock.recv()
+    def trigger(self):
+
+        #Todo - convert this to pub-sub
+        self.sock.send("trig")
+        print self.sock.recv()
+        self.logic()
+
+    def logic(self):
+
+        data = fo.read().split("\n")
+
+        for i in data:
+           field = i.split(" ")
+           if len(field) > 1:
+               if str(field[1]) == 'B':
+                   wf = " ".join(field)
+                   wf = wf + "\n"
+                   fo2.write(wf)
+               else:
+                   wf = " ".join(field)
+                   wf = wf + "\n"
+                   sock.send("msg:%s"%wf)
+                   print("sent ( %s )to node 1"%wf)
+                   print sock.recv()
+
+
+if __name__ == "__main__":
+    c = ClientCode()
+    c.trigger()
--- node2/server2.py
+++ node2/server2.py
@@ -1,4 +1,5 @@
 import zmq
+from client1 import *
 
 # ZeroMQ Context
 context = zmq.Context()
@@ -7,18 +8,23 @@ context = zmq.Context()
 sock = context.socket(zmq.REP)
 sock.bind("tcp://*:5556")
 
-fo = open("dataB.txt", "w+")
-
-# Run a simple "Echo" server
 while True:
     message = sock.recv()
     message = message.split(":")
 
-    if message[0] == "msg":
+    if message[0] == "trig":
+        c = ClientCode()
+        c.trigger()
+        sock.send("started")
+
+    elif message[0] == "msg":
+        fo = open("dataB.txt", "a+")
         content = message[1]
         fo.write(content)
         sock.send("done")
         print "done"
+        fo.close()
+
     else:
         print("Please send again!!")
         sock.send("again") 
