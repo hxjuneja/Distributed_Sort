17de38f9cf3af26d30e38d5979201d67e3bc2563
hardikj <hardikjuneja.hj@gmail.com>
2014-05-05 03:42:29 +0530

    minor changes in server code

--- node1/server.py
+++ node1/server.py
@@ -1,15 +1,30 @@
 import zmq
 
+# ZeroMQ Context
 context = zmq.Context()
 
 # Define the socket using the "Context"
 sock = context.socket(zmq.REP)
 sock.bind("tcp://*:5555")
 
-# Run a simple "Echo" server
 while True:
     message = sock.recv()
-    print("got:%s "%message)
-    
-    sock.send("hey, repling ya!!")
-    print "Sent: " + message
+    message = message.split(":")
+
+    if message[0] == "trig":
+        c = ClientCode()
+        c.trigger()
+        sock.send("started")
+
+    if message[0] == "msg":
+
+        fo = open("dataA.txt", "w+")
+        content = message[1]
+        fo.write(content)
+        sock.send("done")
+        print "done"
+        fo.close()
+
+    else:
+        print("Please send again!!")
+        sock.send("again") 
