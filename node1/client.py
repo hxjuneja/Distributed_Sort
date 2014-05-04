716e0bba6d2a64cca0a99d04606ea5ceecde34f2
hardikj <hardikjuneja.hj@gmail.com>
2014-05-04 22:18:10 +0530

    adding new directories and some more server code

---
+++ node1/client.py
@@ -0,0 +1,29 @@
+import zmq
+import sys
+
+context = zmq.Context()
+
+# server 1
+sock = context.socket(zmq.REQ)
+sock.connect("tcp://172.16.86.44:5555")
+
+# Open file
+fo = open("data1.txt", "r+")
+fo2 = open("dataA.txt", "w+")
+
+data = fo.read().split("\n")
+
+for i in data:
+    field = i.split(" ")
+    if len(field) > 1:
+        if str(field[1]) == 'A':
+             wf = " ".join(field)
+             wf = wf + "\n"
+             fo2.write(wf)
+        else:
+             wf = " ".join(field)
+             wf = wf + "\n"
+             sock.send("Sending")
+             print("sent to client one")
+             
+             print sock.recv()
