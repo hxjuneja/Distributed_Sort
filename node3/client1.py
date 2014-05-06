4286bc782961620eeb618cdc1c5ad51ecfe14630
hardikj <hardikjuneja.hj@gmail.com>
2014-05-06 22:46:03 +0530

    added code for 3rd node

---
+++ node3/client1.py
@@ -0,0 +1,45 @@
+import zmq
+import sys
+
+
+class ClientCode():
+    
+    def __init__(self):
+        context = zmq.Context()
+
+        # connect to server
+        self.sock = context.socket(zmq.REQ)
+        self.sock.connect("tcp://localhost:5555")
+
+        # Open file
+        self.fo = open("../data/data1.txt", "r+")
+        self.fo2 = open("dataC.txt", "a+")
+
+    def trigger(self):
+
+        #Todo - convert this to pub-sub
+        self.sock.send("trig")
+        self.sock.recv()
+        self.logic()
+
+    def logic(self):
+
+        data = self.fo.read().split("\n")
+
+        for i in data:
+           field = i.split(" ")
+           if len(field) > 1:
+               if str(field[1]) == 'C':
+                   wf = " ".join(field)
+                   wf = wf + "\n"
+                   self.fo2.write(wf)
+               else:
+                   wf = " ".join(field)
+                   wf = wf + "\n"
+                   self.sock.send("msg:%s"%wf)
+                   print("sent ( %s )to node 1"%wf)
+                   print self.sock.recv()
+
+if __name__ == "__main__":
+    c = ClientCode()
+    c.logic()
